b094e3af682ca87edb08f6c3c692f669375792f0
Eduardo S. Libardi <eslibardi@gmail.com>
2025-09-29 11:23:12 -0300

    Remove pexels from next-config
    Change package.json version to 1.0.0

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,6 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.pexels.com',
-      },
-      {
-        protocol: 'https',
         hostname: 'img.clerk.com',
       },
       {
